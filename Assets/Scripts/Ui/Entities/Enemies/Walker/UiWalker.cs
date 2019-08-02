5f1c51bd205aca37817fd25789c8cdc814e6a87f
ycarowr <ywrycaro@gmail.com>
2019-08-03 01:01:23 +0300

    Add walker enemy

---
+++ Assets/Scripts/Ui/Entities/Enemies/Walker/UiWalker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SpaceMarine.Data;
+using SpaceMarine.Model;
+
+
+namespace SpaceMarine
+{
+    public partial class UiWalker : UiEnemy
+    {
+        public override void OnCollideBullet(UiBullet bullet)
+        {
+            var damage = bullet.Damage;
+            TryTakeDamage(damage);
+        }
+    }
+}
