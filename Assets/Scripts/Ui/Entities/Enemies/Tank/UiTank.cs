d0e83c23eb89b4abf42116673ae667a2131f1976
ycarowr <ywrycaro@gmail.com>
2019-08-03 00:58:36 +0300

    Add Tank enemy

---
+++ Assets/Scripts/Ui/Entities/Enemies/Tank/UiTank.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SpaceMarine.Data;
+using SpaceMarine.Model;
+
+
+namespace SpaceMarine
+{
+    public partial class UiTank : UiEnemy
+    {
+        public override void OnCollideBullet(UiBullet bullet)
+        {
+            var damage = bullet.Damage;
+            TryTakeDamage(damage);
+        }
+    }
+}
